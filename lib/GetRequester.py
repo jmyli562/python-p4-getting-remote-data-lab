71830b1acbe900aad349284b6ba2f8d616794907
Jimmy Li <60550632+jmyli562@users.noreply.github.com>
2023-09-02 16:36:45 -0700

    Update GetRequester.py

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -1,13 +1,22 @@
 import requests
 import json
 
-class GetRequester:
 
+class GetRequester:
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        url = self.url
+
+        response = requests.get(url)
+
+        return response.content
 
     def load_json(self):
-        pass
+        employee_list = []
+        employees = json.loads(self.get_response_body())
+        for employee in employees:
+            employee_list.append(employee)
+
+        return employee_list
